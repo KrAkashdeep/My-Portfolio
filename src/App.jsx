3690b1f4b1ef66e4931cf22b7740c9552d59a90f
KrAkashdeep <akashdeep93140@gmail.com>
2025-01-25 14:21:37 +0530

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,6 @@
-// // import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
-// import Home from "./components/Home";
-// import About from "./components/About";
-// import Project from "./components/Project";
-// import Contact from "./components/Contact";
-// import Skills from "./components/Skills";
 import Page from "./Page";
-// import Footer from "./components/Footer";
-
-// const App = () => {
-//   return (
-//     <Router>
-//       <main>
-//         <Navbar />
-//         <Routes>
-//           <Route path="/" element={<Page />} />
-//           <Route path="/" element={<Home />} />
-//           <Route path="/about" element={<About />} />
-//           <Route path="/projects" element={<Project />} />
-//           <Route path="/skills" element={<Skills />} />
-//           <Route path="/contact" element={<Contact />} />
-//           <Route path="/footer" element={<Footer />} />
-//         </Routes>
-//       </main>
-//     </Router>
-//   );
-// };
 
 const App = () => {
   return (
